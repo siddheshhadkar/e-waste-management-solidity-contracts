691f0807ae7d7644f3678d42f550904a79bd1683
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2020-01-30 19:40:49 +0530

    Update CollectionCentre.sol

--- CollectionCentre.sol
+++ CollectionCentre.sol
@@ -8,8 +8,10 @@ contract CollectionCentre{
 	mapping(uint => CollectionCentreDetails) CollectionCentres;
     uint public CollectionCentreCount;
     address public producerContractAddress;
+    address public RecycleUnitContractAddress;
     address owner;
     Producer producerInstance;
+    RecycleUnit RUInstance;
 
     modifier onlyOwner(){
         require(msg.sender == owner);
@@ -34,5 +36,18 @@ contract CollectionCentre{
     function getProducerAddress() public onlyOwner{
         producerContractAddress = am.getAddress("Producer");
         producerInstance = Producer(address(producerContractAddress));
+    }
+
+    function getRecycleUnitAddress() public onlyOwner{
+        RecycleUnitContractAddress = am.getAddress("RecycleUnit");
+        RUInstance = RecycleUnit(address(RecycleUnitContractAddress));
+    }
+
+    function receiveItemProducer() public{
+
+    }
+    function sendItemRecycleUnit() public{
+
+    
     }
 }
